f007e85ca923c9611a61e5ba3234ea07fb990295
malodev <mlongano@gmail.com>
2012-07-06 16:52:49 +0200

    corretto un typo

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-setup(name='soarign_club',
+setup(name='soaring_club',
       version='0.1',
       packages=find_packages(),
-      package_data={'soarign_club': ['bin/*.*', 'static/*.*', 'templates/*.*']},
-      exclude_package_data={'soarign_club': ['bin/*.pyc']},
-      scripts=['soarign_club/bin/manage.py'])
+      package_data={'soaring_club': ['bin/*.*', 'static/*.*', 'templates/*.*']},
+      exclude_package_data={'soaring_club': ['bin/*.pyc']},
+      scripts=['soaring_club/bin/manage.py'])
